aa537b8f6846605bf9c76a177053551e378206c9
Clestxn <clestonkenji@hotmail.com>
2024-04-02 11:30:19 -0300

    Aula2603 e 0204 - Renderizadores com Hooks

--- basic/basictools/src/components/Form/Form.jsx
+++ basic/basictools/src/components/Form/Form.jsx
@@ -1,44 +1,45 @@
-import React from 'react'
+import React, {useState} from 'react'
+
 var teste = 8;
+
 const Form = () => {
-    const handleMyEvent = (e) =>{
-      console.log(e)
-      console.log("Ativou o Evento!")
-    }
-const redeButton = (x) =>{
-  if(x){
-        return <h1>Renderizou bonitinho!</h1>
-  }else{
-        return <h1>Rendelizou Lindinho!!!</h1>
-      }
-    }
+  const handleMyEvent = () => {
+    console.log("Ativou o Evento!")
+  }
+
+const [x, setX] = useState(true)
   return (
     <>
     <div>
       <div>
-        <button onClick={handleMyEvent}>click aqui</button>
+        <button onClick={handleMyEvent}>Clique Aqui</button>
       </div>
+
       <div>
-        <button onClick={() =>{console.log("Yes! Apertou em mim.!!!")}}>Click em mim também</button>
+        <button onClick={() => {console.log("Yes! Apertou em mim!")}}>Click em mim também</button>
       </div>
+
       <div>
         <button onClick={() => {
           if (true){
             console.log("Isso não deveria ter acontecer!")
-            }
+          }
         }}>Será que vocÊ clicaria aqui</button>
       </div>
-      <div>
-        {redeButton(true)}
-        {redeButton(false)}
 
+      <div>
+          <h1>{x ? 'Renderizou' : 'Deu ruim, não foi'}</h1>
+        <button onClick={() => setX(!x)}>Valida X</button>
       </div>
+
       <div>
         <strong>Valor:</strong>{teste}
         <button onClick={() => {teste = 12; console.log(teste)}}>Aperte e mude</button>
         <strong>Valor:</strong>{teste}
       </div>
+
     </div>
+
     </>
   )
 }
