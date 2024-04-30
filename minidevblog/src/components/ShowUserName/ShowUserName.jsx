e2dab0fc900de56616d6a807767d29e72e447391
Davidfs305 <david.salles@fatec.sp.gov.br>
2024-04-30 12:27:35 -0300

    Avaliação - David Francisco Salles

---
+++ minidevblog/src/components/ShowUserName/ShowUserName.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ShowUserName = () => {
+  return (
+    <>
+      <div>
+        <h2>ShowUserName </h2>
+      </div>
+    </>
+  )
+}
+
+export default ShowUserName
---
+++ minidevblog/src/firebase/config.jsx
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app"
+import { getAnalytics } from "firebase/analytics"
+import { getFirestore } from "firebase/firestore"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCWITSopuKDfk2FXDJfSscRYmQ1nzE83eY",
+    authDomain: "minidevblogfelipe.firebaseapp.com",
+    projectId: "minidevblogfelipe",
+    storageBucket: "minidevblogfelipe.appspot.com",
+    messagingSenderId: "344657334758",
+    appId: "1:344657334758:web:0a4faf38be15839246c627",
+    measurementId: "G-1J8RG9864H"
+  }
+
+  const app = initializeApp(firebaseConfig)
+  const analytics = getAnalytics(app);
+  const db = getFirestore(app)
+
+  export{db}
