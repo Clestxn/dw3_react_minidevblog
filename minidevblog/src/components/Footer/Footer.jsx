7cdb1a5bb032e0d2903b7b066cf7ef85bb591cab
Clestxn <cleston.tonooka@fatec.sp.gov.br>
2024-04-30 12:25:34 -0300

    Avaliação - Cleston Kenji Maruyama Tonooka

---
+++ minidevblog/src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './Footer.module.css'
+
+const Footer = () => {
+    return(
+        <>
+            <footer className={styles.footer}>
+                <h3>Avaliação DW3</h3>
+                <p>Blog Dev</p>
+            </footer>
+        </>
+    )
+}
+
+export default Footer
